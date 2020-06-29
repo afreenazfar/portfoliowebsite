d44d80aeeb6df107650a7ebcd274b46144dbb365
Afreen Azfar <aa253@students.waikato.ac.nz>
2020-06-29 12:04:05 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Content } from 'react-mdl';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* Uses a transparent header that draws on top of the layout's background */}
+<div style={{height: '550px', width: 'auto',  position: 'relative'}}>
+    <Layout style={{background: 'url(https://i.imgur.com/QIrgvF7.png) center / cover'}}>
+        <Header className="header" title="Afreen Azfar ">
+            <Navigation>
+                <a href="/">About Me</a>
+                <a href="/">Projects</a>
+                <a href="/">Progress</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        {/* <Drawer title="Afreen">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer> */}
+        <Content />
+    </Layout>
+</div>
     </div>
   );
 }
