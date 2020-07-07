6e74596476b8126ddb207eed04442bb4afb6e711
Afreen Azfar <aa253@students.waikato.ac.nz>
2020-07-07 18:27:48 +1200

    Updates

---
+++ precache-manifest.4d54b971cd1a56ea590cee0842f5fbc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3098c1d2be148240d5df745d36db8268",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9167ddb82c65a7de7b5b",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e280e3b1e419ca1e694a",
+    "url": "/static/css/main.500f1ca7.chunk.css"
+  },
+  {
+    "revision": "9167ddb82c65a7de7b5b",
+    "url": "/static/js/2.163dacaf.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.163dacaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e280e3b1e419ca1e694a",
+    "url": "/static/js/main.c61fc0db.chunk.js"
+  },
+  {
+    "revision": "0d17cf16d618cde135c2",
+    "url": "/static/js/runtime-main.e2b9061a.js"
+  }
+]);
