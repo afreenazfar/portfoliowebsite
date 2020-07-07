d10be2f212a923e4dae0b8a4921ecce0e44bc4d8
Afreen Azfar <aa253@students.waikato.ac.nz>
2020-07-08 11:11:21 +1200

    positioning of LP changed

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell,} from 'react-mdl';
 
 class LandingPage extends Component {
     render() {
@@ -10,11 +10,13 @@ class LandingPage extends Component {
                      
                     <Cell col={12}> 
                     <div className="banner-text">
-                    <img src="https://i.imgur.com/jbgXIMg.png" alt="avatar" className="avatarImg"/> 
-                        <h2 style={{paddingTop:'15px' , paddingBottom:'-50px'}}>Afreen Azfar</h2> 
-                        <h1>MSc (Biology), PGCert (Information Technology)</h1>
+                    <div className="lp-text">
+                        <img src="https://i.imgur.com/yIK3G5i.png" alt="avatar" className="avatarImg"/> 
+                        <h2 style={{paddingTop:'3vh' , paddingBottom:'-50px'}}>Afreen Azfar</h2> 
+                        <h4>MSc (Biology), PGCert (Information Technology)</h4>
                         <p>HTML | CSS | JavaScript | React | Java | SQL | Git</p>
-                        <div className="social-links" ></div>
+                        </div>
+                        <div className="social-links"></div>
                         {/*LinkedIn */}
                         <a href="https://www.linkedin.com/in/afreen-azfar?originalSubdomain=nz" rel="noopener noreferrer" target="_blank"> 
                         <i className="fa fa-linkedin-square" aria-hidden="true" /> </a>
